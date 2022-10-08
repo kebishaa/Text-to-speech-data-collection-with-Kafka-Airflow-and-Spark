9ee5bedf1ca36256a8c07d91cc90f99927dfb264
kebishaa <kbkibatu@gmail.com>
2022-10-08 23:13:02 +0300

    added sentence publisher

--- Flask/sentence_publisher.py
+++ Flask/sentence_publisher.py
@@ -4,3 +4,8 @@ from kafka_utils import KafkaUtils
 import pandas as pd
 df = pd.read_csv("../data/amharic_news_dataset.csv")
 producer = KafkaUtils.create_producer()
+for row in df.itertuples():
+    producer.send("g5-untranscribed-text", value=row.article)
+    print(type(row.article))
+    print(row.article)
+    # break
