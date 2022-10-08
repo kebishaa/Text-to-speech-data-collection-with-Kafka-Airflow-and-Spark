6bb3e22fd623e7966f8b84561393e155f58b05bc
kebishaa <kbkibatu@gmail.com>
2022-10-08 23:11:43 +0300

    added read csv fun for consumer_message

--- Flask/sentence_publisher.py
+++ Flask/sentence_publisher.py
@@ -2,3 +2,5 @@ import sys
 sys.path.append("../scripts")
 from kafka_utils import KafkaUtils
 import pandas as pd
+df = pd.read_csv("../data/amharic_news_dataset.csv")
+producer = KafkaUtils.create_producer()
